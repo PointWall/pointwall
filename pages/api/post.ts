47252ff48781b058031844d29ce1e5c6e2f5d4a1
r4n <ramiroreinaldo12@gmail.com>
2023-05-21 23:36:09 -0300

    💾 Create basic post endpoints for Post and User

---
+++ pages/api/post.ts
@@ -0,0 +1,43 @@
+import { prisma } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+//import { getServerSession } from "next-auth/next";
+//import { authOptions } from "./auth/[...nextauth]";
+
+export default async function handler(req: NextRequest, res: NextResponse) {
+  //const session = await getServerSession(req, res, authOptions));
+  if (req.method == "POST") {
+  //if(!session) return res.status(401).json({ error: 'Not Authorized'  })
+
+    const { title, images, authorId, location, artType, userType } = req.body;
+    console.log(req.body);
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        images: images,
+        author: {
+          connect: {
+            id: authorId,
+          },
+        },
+        artType,
+        userType,
+        location
+      },
+    });
+
+    return res.status(200).json({ newPost });
+  }
+
+  if (req.method == "GET") {
+    const posts = await prisma.post.findMany()
+    return res.status(200).json({ posts });
+  }
+
+//  if (req.method == "PUT") {
+//    return res.status(200).json({ message: "ok" });
+//  }
+
+//  if (req.method == "DELETE") {
+//    return res.status(200).json({ message: "ok" });
+//  }
+}
---
+++ pages/api/user.ts
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function handler(req: NextRequest, res: NextResponse) {
+  if (req.method == "POST") {
+    const { firstName, lastName, email, isAdmin } = req.body;
+    console.log(req.body);
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        posts: {},
+        email,
+        isAdmin,
+      },
+    });
+    return res.status(200).json({ newUser });
+  }
+
+//  if (req.method == "GET") {
+//    return res.status(200).json({ message: "ok" });
+//  }
+
+  //  if (req.method == "PUT") {
+  //    return res.status(200).json({ message: "ok" });
+  //  }
+
+  //  if (req.method == "DELETE") {
+  //    return res.status(200).json({ message: "ok" });
+  //  }
+}
