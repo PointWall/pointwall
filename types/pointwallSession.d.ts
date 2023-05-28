40893d729d39235f80617982fba7c3e9250ffeed
r4n <ramiroreinaldo12@gmail.com>
2023-05-28 00:44:36 -0300

    📝 Create pointwallSession Type

---
+++ types/pointwallSession.d.ts
@@ -0,0 +1,9 @@
+// nextauth.d.ts
+import { DefaultSession } from "next-auth";
+import type { User } from '@prisma/client'
+
+
+  
+interface PointwallSession extends DefaultSession {
+  user?: User;
+}
