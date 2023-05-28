66251de06e49ddeb5819e7957af4c476e62fb4c9
r4n <ramiroreinaldo12@gmail.com>
2023-05-28 00:45:47 -0300

    🛠️ Implement nextAuth Google provider with DB

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,24 +1,55 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/db";
+import { DefaultSession } from "next-auth";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      
-    })
-    // ...add more providers here
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
   callbacks: {
-    session: async ({ session, token, user }: { session:any, token:any, user:any }) => {
+    session: async ({
+      session,
+    }: {
+      session: DefaultSession;
+
+    }) => {
+      console.log(session);
       if (session?.user) {
-        session.user.id = user.id;
+        //Check if exist, if not create user
+        let user = await prisma.user.findUnique({
+          where: {
+            email: session?.user?.name || "",
+          },
+        });
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              firstName: session?.user?.name?.split(" ")[0] || "",
+              lastName: session?.user?.name?.split(" ")[1] || "",
+              posts: {},
+              email: session?.user?.email || "",
+              isAdmin: false,
+            },
+          });
+          console.log("User Created!");
+        }
+        session.user = user;
       }
+      console.log({session})
       return session;
     },
   },
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
