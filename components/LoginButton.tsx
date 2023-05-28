a0bb6dd8b29485420b06330440bd7283b1c9b3ad
r4n <ramiroreinaldo12@gmail.com>
2023-05-28 00:49:39 -0300

    💅Adding style and implementing PointwallSession type in Loginbutton.tsx

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,23 +1,29 @@
-import { useSession, signIn, signOut } from 'next-auth/react'
+import { useSession, signIn, signOut } from "next-auth/react";
+import { PointwallSession } from "@/types/pointwallSession";
 
-export default function Component (): JSX.Element {
-  const { data: session } = useSession()
+export default function Component(): JSX.Element {
+  const { data: session } = useSession();
+  const pointwallSession = session as PointwallSession;
 
-  function handleLogin (): void {
-   signIn('google').catch(console.error)
+  function handleLogin(): void {
+    signIn("google").catch(console.error);
   }
 
-  function handleLogout (): void {
-    signOut().catch(console.error)
+  function handleLogout(): void {
+    signOut().catch(console.error);
   }
 
   if (session != null) {
     return (
-      <>
-        Bienvenid@, {session.user?.name} <br />
+      <section className="text-lg flex center items-center flex-center gap-2">
+        <p className="font-mediumt">{pointwallSession?.user?.email}</p>
         <button onClick={handleLogout}>Salir</button>
-      </>
-    )
+      </section>
+    );
   }
-  return <button onClick={handleLogin}>Iniciar sesión</button>
+  return (
+    <button className="text-lg" onClick={handleLogin}>
+      Iniciar sesión
+    </button>
+  );
 }
