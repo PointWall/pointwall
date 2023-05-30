81acf2c3b2c1600d8a04e892d9efce06f533fa75
r4n <ramiroreinaldo12@gmail.com>
2023-05-29 22:58:53 -0300

    🖼  Import origen.tsx page

--- pages/origen.tsx
+++ pages/origen.tsx
@@ -1,11 +1,52 @@
-import Layout from '@/components/Layout'
+import Layout from "@/components/Layout";
+import Image from 'next/image'
 
-export default function Page (): JSX.Element {
+const EXAMPLES = [
+  {
+    name: "España",
+    description:
+      "En la Sala de la Gran Pintura, la cual es la más espectacular en cuanto a los frescos existentes en la mansión, se encuentran pinturas son siglo I AC, que hacen referencia a la iniciación de las mujeres en el culto a Dionisio.",
+    img: "/images/mural_españa.jpg",
+  },
+  {
+    name: "Italia",
+    description:
+      "Miguel Ángel decoró por completo la bóveda de este espacio, con un conjunto de frescos que representan un enorme número de escenas bíblicas y en el que se citan más de 300 personajes distintos en unos 500 metros cuadrados de pintura. El pasaje de la Creación de Adán debe ser uno de los más famosos de la Historia del Arte.",
+    img: "/images/mural_italia.jpg",
+  },
+  {
+    name: "Estonia",
+    description:
+      "En Pärnu en Estonia, el cual muestra la ambición por las cosas materiales y superficiales se ha posicionado como el motor de la vida en el siglo XXI. Este mural y otros se pueden encontrar al caminar junto al río Pärnu, muy cerca del puente que va hacia la bella ciudad de Tallin, la capital de Estonia.",
+    img: "/images/mural_estonia.jpg",
+  },
+];
+
+export default function Page(): JSX.Element {
   return (
     <Layout>
-      <section className='text-center'>
-        <h1 className='text-8xl mt-16'>Orígen Histórico</h1>
+      <div className="text-center">
+        <h1 className="text-8xl mt-16">Orígenes Históricos</h1>
+      </div>
+
+      <section className="flex align-items justify-center gap-2 p-3 m-5">
+        {EXAMPLES.map((example) => (
+          <div key={example.name} className="border-black rounded-md">
+            <div className="">
+              <h3 className="text-center blonde font-bold">{example.name}</h3>
+              <div className="flex align-items justify-center items-center gap-5 p-5 flex-center flex-col ">
+                <p>{example.description}</p>
+                <Image
+                      style={{width: "75%", height: "100%"}}
+                      src={example.img ?? '/images/foto_default.png'}
+                      alt={`Imagen de ${example.name}`} width={100} height={100}
+                      className='w-36 h-36 border-4 border-black shadow-lg shadow-zinc-400 rounded-sm'
+                    />
+              </div>
+            </div>
+          </div>
+        ))}
       </section>
     </Layout>
-  )
+  );
 }
