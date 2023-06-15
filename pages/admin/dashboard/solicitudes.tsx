0f579b8ee22bcb05d896964b880bd542b31dcfad
Gino S <ginossomi@gmail.com>
2023-06-15 11:44:53 -0300

    🔑 Add key prop

--- pages/admin/dashboard/solicitudes.tsx
+++ pages/admin/dashboard/solicitudes.tsx
@@ -2,20 +2,26 @@ import AdminLayout from '@/components/AdminLayout'
 import { prisma } from '@/lib/db'
 import { Post } from '@prisma/client'
 
-export async function getStaticProps() {
-  const posts = await prisma.post.findMany()
-  return {
-    props: {
-      posts
+export async function getStaticProps () {
+  try {
+    const posts = await prisma.post.findMany()
+    return {
+      props: {
+        posts
+      }
+    }
+  } catch (err) {
+    return {
+      props: {}
     }
   }
 }
 
-export default function Page({ posts }: { posts: Post[] }): JSX.Element {
+export default function Page ({ posts }: { posts: Post[] }): JSX.Element {
   return (
     <AdminLayout title='Solicitudes'>
       <p>Acá se muestran las solicitudes generadas por usuarios</p>
-      {posts.length > 0 ? <div>{posts.map((post) => (<div>post</div>))}</div> : <p>De momento no hay solicitudes de colaboración...</p>}
+      {posts.length > 0 ? <div>{posts.map((post, i) => (<div key={i}>post</div>))}</div> : <p>De momento no hay solicitudes de colaboración...</p>}
     </AdminLayout>
   )
 }
