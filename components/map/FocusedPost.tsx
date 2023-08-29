20c9468a50b3a86c7b074b9cbb002b0871724d20
Lucas <lucaspiputto@gmail.com>
2023-08-29 10:39:45 -0300

    🗺 Update according to branch main

--- components/map/FocusedPost.tsx
+++ components/map/FocusedPost.tsx
@@ -8,7 +8,7 @@ export default function FocusedPost ({
   post: Post
 }): JSX.Element {
   return (
-    <div className='max-w-xs container bg-white rounded-xl shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl'>
+    <div className='max-w-xs container bg-white rounded-xl shadow-lg'>
       <div>
         <button
           onClick={close}
@@ -17,7 +17,7 @@ export default function FocusedPost ({
           X
         </button>
         <h1 className='text-2xl py-2 ml-4 font-bold text-gray-800 hover:text-gray-900 transition duration-100'>
-          {post.title !== undefined ? post.title : 'Sin título'}
+          {post.title ?? 'Sin título'}
         </h1>
       </div>
       <img
