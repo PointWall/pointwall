4921d1284a5036cefdec8a1930fbeb3dc8ef3f1f
Lucas <lucaspiputto@gmail.com>
2023-08-30 16:09:58 -0300

    ❔ Add help text

--- pages/contribucion.tsx
+++ pages/contribucion.tsx
@@ -5,13 +5,16 @@ import { useState, FormEvent, ChangeEvent } from 'react'
 import Head from 'next/head'
 import Layout from '@/components/Layout'
 import { Title, Wrapper } from '@/components/utils'
-import NotLogged from '@/components/NotLogged'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons'
+// import NotLogged from '@/components/NotLogged'
 
 type inputValue = 'title' | 'description' | 'location' | 'tags'
 
 interface InputOp {
   value: inputValue
   text: string
+  helpText?: string
   placeholder: string
 }
 
@@ -50,22 +53,25 @@ const USER_TYPE_OPTIONS = [
 const TEXT_INPUTS: InputOp[] = [
   {
     value: 'location',
-    text: '¿Dónde queda? Agregá la longitud y la latitud (Ej: 12.123123, 11.15123)',
+    text: '¿Dónde queda?',
+    helpText: 'Agregá la información más precisa que tengas de la ubicación. Puede ser dirección, calle que corta, o hasta longitud y latitud. También podés agregar un link de google maps referenciando la ubicación.',
     placeholder: '12.123123, 11.15123'
   },
   {
     value: 'title',
-    text: '¿Qué nombre le pondrías a la ubicación? (¡Cuanto más claro sea más posibilidades existen de que la colaboración sea publicada!)',
+    text: '¿Qué nombre le pondrías a la ubicación?',
+    helpText: 'Cuanto más claro sea más posibilidades existen de que la colaboración sea publicada',
     placeholder: 'Mural Benito'
   },
   {
     value: 'description',
-    text: '¿Querés agregarle algun comentario/descripción? ',
+    text: '¿Querés agregarle algún comentario/descripción? ',
     placeholder: 'Obra renovada, de gran importancia barrial...'
   },
   {
     value: 'tags',
-    text: '¿Le agregarías alguna etiqueta descriptiva (artista, lugar, estilo, etc...)?',
+    text: '¿Le agregarías alguna etiqueta descriptiva?',
+    helpText: 'Las etiquetas pueden estar relacionadas a: contenido, lugar, estilo, etc...',
     placeholder: 'Fútbol, Arte moderno, Homenaje...'
   }
 ]
@@ -73,18 +79,19 @@ const TEXT_INPUTS: InputOp[] = [
 interface InputSectionProps {
   children: JSX.Element | JSX.Element[]
   title: string
+  helpText?: string
 }
 
-async function saveImages(fileInput: FileList | undefined) {
-  if (!fileInput) return []
+async function saveImages (fileInput: FileList | undefined): Promise<any[]> {
+  if (fileInput == null) return []
 
-  let savedImages: String[] = []
-  let files = Array.from(fileInput)
+  const savedImages: String[] = []
+  const files = Array.from(fileInput)
 
   const formData = new FormData()
   formData.append('upload_preset', 'pointwall')
-  formData.append('api_key',process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY ?? '')
-  
+  formData.append('api_key', process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY ?? '')
+
   for (const file of files) {
     formData.append('file', file)
     try {
@@ -92,7 +99,7 @@ async function saveImages(fileInput: FileList | undefined) {
         'https://api.cloudinary.com/v1_1/dsxmh7gww/image/upload',
         {
           method: 'POST',
-          body: formData,
+          body: formData
         }
       )
       savedImages.push((await data.json()).url)
@@ -104,16 +111,25 @@ async function saveImages(fileInput: FileList | undefined) {
   return savedImages
 }
 
-function InputSection({ children, title }: InputSectionProps): JSX.Element {
+function InputSection ({ children, title, helpText }: InputSectionProps): JSX.Element {
   return (
     <div className='border-b-2 border-slate-700 pb-4 last:border-b-0 md:border-b-0 md:border-l-4 md:pb-0 md:pl-4'>
-      <h3 className='mb-2 text-xl'>{title}</h3>
+      <h3 className='mb-2 text-xl'>
+        {title}
+        {
+        helpText !== undefined &&
+          <span className='relative ml-2 text-gray-400 rounded-full border w-5 h-5 inline-flex items-center justify-center hover:bg-gray-100 group'>
+            <FontAwesomeIcon icon={faQuestion} className='block text-xs' />
+            <span className='absolute w-56 left-full ml-2 hidden group-hover:block text-xs bg-white border text-gray-800 rounded p-2 shadow-lg animate-[slideRight_0.3s_ease] z-10'>{helpText}</span>
+          </span>
+        }
+      </h3>
       {children}
     </div>
   )
 }
 
-export default function Page(): JSX.Element {
+export default function Page (): JSX.Element {
   const { data: session } = useSession()
   const pointwallSession = session as PointwallSession
   const [images, setImages] = useState<FileList>()
@@ -123,32 +139,28 @@ export default function Page(): JSX.Element {
   const [formData, setFormData] = useState(FORM_INITIAL_STATE)
   // const [isOtherChecked, setIsOtherChecked] = useState(false)
 
-  async function postContribution(): Promise<void> {
+  async function postContribution (): Promise<void> {
     if (pointwallSession?.user == null) {
-     alert('Debes iniciar sesión para poder enviar el formulario')
-      return 
+      alert('Debes iniciar sesión para poder enviar el formulario')
+      return
     }
-    formData.author = { id: pointwallSession?.user?.id}
+    formData.author = { id: pointwallSession?.user?.id }
     formData.images = await saveImages(images)
 
     await fetch('/api/post', {
       method: 'POST',
       body: JSON.stringify({ ...formData }),
       headers: {
-        'content-type': 'application/json',
-      },
+        'content-type': 'application/json'
+      }
     }).catch((e) => console.log(e))
   }
 
-  async function handleSubmit(ev: FormEvent) {
+  function handleSubmit (ev: FormEvent): void {
     ev.preventDefault()
-    postContribution().catch((error) => console.error(error)).then(_ => setFormData(FORM_INITIAL_STATE))
+    postContribution().then(_ => setFormData(FORM_INITIAL_STATE)).catch((error) => console.error(error))
   }
 
-  // function handleInputChange (ev: ChangeEvent & { target: HTMLInputElement }): void {
-
-  // }
-  
   return (
     <>
       <Head>
@@ -159,13 +171,10 @@ export default function Page(): JSX.Element {
           <Title>Formulario de contribución</Title>
         </section>
         <section className='my-16 mb-8 accent-slate-700'>
-         { pointwallSession?.user != null ? <Wrapper>
-            <form
-              onSubmit={(ev) => {
-                handleSubmit(ev)
-                return false
-              }}
-            >
+          {/* {pointwallSession?.user != null
+            ?  */}
+          <Wrapper>
+            <form onSubmit={handleSubmit}>
               <div className='flex flex-col gap-6'>
                 <InputSection title='¿Qué tipo de usuario sos?'>
                   <div className='ml-4'>
@@ -209,7 +218,7 @@ export default function Page(): JSX.Element {
                             artType: ev.target.value
                           }))
                         }}
-                        checked={formData.artType === "Mural"}
+                        checked={formData.artType === 'Mural'}
                       />
                       <label htmlFor='mural'>Mural</label>
                     </div>
@@ -227,7 +236,7 @@ export default function Page(): JSX.Element {
                             artType: ev.target.value
                           }))
                         }}
-                        checked={formData.artType === "Graffiti"}
+                        checked={formData.artType === 'Graffiti'}
                       />
                       <label htmlFor='graffiti'>Graffiti</label>
                     </div>
@@ -242,10 +251,10 @@ export default function Page(): JSX.Element {
                         ) {
                           setFormData((prevData) => ({
                             ...prevData,
-                            artType: ""
+                            artType: ''
                           }))
                         }}
-                        checked = {!["Graffiti", "Mural"].includes(formData.artType)}
+                        checked={!['Graffiti', 'Mural'].includes(formData.artType)}
                       />
                       <label htmlFor='other'>Otro: </label>
                       <input
@@ -261,33 +270,32 @@ export default function Page(): JSX.Element {
                             }
                           })
                         }}
-                        value={!["Graffiti", "Mural"].includes(formData.artType) ? formData.artType : ""}
+                        value={!['Graffiti', 'Mural'].includes(formData.artType) ? formData.artType : ''}
                         className='w-full max-w-xs border-b-2 bg-slate-50 px-[.5em] outline-none focus:border-slate-400 focus:bg-slate-100'
                       />
                     </div>
                   </div>
                 </InputSection>
                 {TEXT_INPUTS.map((input) => (
-                  <InputSection key={input.value} title={input.text}>
+                  <InputSection key={input.value} title={input.text} helpText={input.helpText}>
                     <input
                       type='text'
                       className='w-full max-w-lg border-b-2 bg-slate-50 px-[.5em] py-[.25em] outline-none focus:border-slate-400 focus:bg-slate-100'
                       onChange={function (ev) {
                         const newData = formData
                         newData[input.value] = ev.target.value
-                        setFormData(p => ({...p, ...newData}))
+                        setFormData(p => ({ ...p, ...newData }))
                       }}
                       /* value={formData[input.value]} */
                       placeholder={`ej: ${input.placeholder}`}
                       value={formData[input.value]}
-                      />
+                    />
                   </InputSection>
                 ))}
                 <InputSection title='¿Tenés fotos que querés que incluyamos en la página?'>
                   <input
                     onChange={(e) =>
-                      setImages(e.currentTarget.files ?? new FileList())
-                    }
+                      setImages(e.currentTarget.files ?? new FileList())}
                     type='file'
                     className='my-2 w-full'
                     multiple
@@ -319,7 +327,8 @@ export default function Page(): JSX.Element {
               </button>
             </form>
           </Wrapper>
-        : <NotLogged />}</section>
+          {/* : <NotLogged />} */}
+        </section>
       </Layout>
     </>
   )
